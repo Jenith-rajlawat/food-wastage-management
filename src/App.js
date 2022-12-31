4a3675620172717a1ad149fa294b059caf4999d6
Jenith Rajlawat <jenithrajlawat@gmail.com>
2023-01-01 00:30:22 +0545

    Final

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from "react"; //We import the useState Hook from React. It lets us keep local state in a function component.
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./components/pages/Home";
 import OurMission from "./components/pages/OurMission";
 import AboutUs from "./components/pages/AboutUs";
 import SignUp from "./components/pages/SignUp";
-import Contact from "./components/Contact";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Home />
-        <OurMission />
-        <AboutUs />
-        <SignUp />
-        <Contact />
+
         <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/ourmission" component={OurMission} />
-          <Route path="/aboutus" component={AboutUs} />
-          <Route path="/sign-up" component={SignUp} />
+          <Route path="/" element={<Home />} />
+          <Route path="/ourmission" element={<OurMission />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>
     </>
